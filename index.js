9a04b8ca12de2d0bc4594fd32d2f9a0fac9977ea
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2023-03-29 16:52:13 +0300

    cretaed first part

---
+++ index.js
@@ -0,0 +1,41 @@
+const formDOM = document.getElementById("form-user");
+const todoList = [];
+
+formDOM.addEventListener("submit", formHandler);
+
+function formHandler(e) {
+    e.preventDefault();
+    addDOM();
+    resetHandler();
+    
+}
+
+const addDOM = () => {   
+    const inputDOM = document.getElementById("gorev");
+    const formListDOM = document.getElementById("form-list");
+
+    if(inputDOM.value !== ""){
+        todoList.push(inputDOM.value);
+
+        const liDOM = document.createElement("li");
+        const todoText = document.createTextNode(inputDOM.value);
+
+        // liDOM.innerHTML=`${inputDOM.value}`;
+        liDOM.appendChild(todoText);
+        formListDOM.appendChild(liDOM);
+        console.log(inputDOM.value); 
+        
+        inputDOM.value="";
+    }  
+}
+
+const resetHandler = function(){
+    const inputDOM = document.getElementById("gorev");
+    const formListDOM = document.getElementById("form-list");
+    const todoText = document.createTextNode(inputDOM.value);
+
+    todoList.pop(inputDOM.value);
+    console.log(inputDOM.value); 
+        
+        inputDOM.value="";
+};
